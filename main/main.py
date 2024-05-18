e808c16a4c10a829e11547d32e9f196ddaa786de
Kuhiro <119740529+MahnThuTa@users.noreply.github.com>
2024-05-18 10:20:34 +0630

    Fixed double loop

--- main/main.py
+++ main/main.py
@@ -9,6 +9,8 @@ import pyttsx3
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
+## System
+
 # Function to capture audio from the microphone
 def capture_audio(output=True, a_speech_time=3):
     with sr.Microphone() as source:
@@ -28,13 +30,13 @@ def transcribe_speech(audio, output=True):
     except sr.UnknownValueError:
         pass
     except sr.WaitTimeoutError:
-        pause()
+        pass
     except sr.RequestError as e:
         script = "Please check your internet connection."
-        print(script)
-        speak(script)
+        print(script, text=True)
         print(f"Error fetching results: {e}")
     return ""
+
 
 # Function to speak to the user
 def speak(script, text=False):
@@ -43,16 +45,66 @@ def speak(script, text=False):
         print(script)
     engine.runAndWait()
 
-# Function to pause the program
-def pause():
-    script = "Paused. Say 'Resume' to continue."
-    speak(script, text=True)
-    while True:
-        audio = capture_audio(output=False)
-        text = transcribe_speech(audio).lower()
-        if "resume" in text:
-            speak("Resuming...", text=True)
-            break
+# Function to check for exit commands
+def is_exit(text):
+    if 'exit' in text or 'quit' in text:
+        script = "Exiting program..."
+        speak(script, text=True)
+        exit()
+
+# Function to check if the text is a math quiz
+def is_math_qz(text):
+    pattern = r"(^\d+\.?(?:\d+)?\s[\+\-\*\/]{1}\s\d+\.?(?:\d+)?$)"
+    if match := re.search(pattern, text):
+        result = eval(match.group(1))
+        speak(f"{match.group(1)} = {result}", text=True)
+
+
+# Function to guide the flow of the program
+def guide_flow(text):
+    pattern = r"(?:open|close|search)\s.+"
+    if match := re.search(pattern, text):
+        if "open" in text:
+            open_app(text)
+        elif "close" in text:
+            close_app(text)
+        elif "search" in text:
+            speak("SEARCHING", text=True)
+            search(text)
+
+# Function to respond to the user
+def respond(start=False):
+    while start:
+        audio_input = capture_audio(output=True)
+        if audio_input:
+            text = transcribe_speech(audio_input).lower()
+            is_exit(text)
+            is_math_qz(text)
+            if "pause" in text or "stop" in text:
+                speak("Paused for a while.", text=True)
+                main()
+                break
+            guide_flow(text)
+
+## Features
+
+# Function to open an application
+def open_app(text):
+    app_name = text.replace("open", "").strip()
+    try:
+        AppOpener.open(app_name, match_closest=True, throw_error=True)
+        speak(f"OPENING {app_name}", text=True)
+    except AppOpener.features.AppNotFound:
+        speak("The application is not on your device.", text=True)
+
+# Function to close an application
+def close_app(text):
+    app_name = text.replace("close", "").strip()
+    try:
+        AppOpener.close(app_name, throw_error=True)
+        speak(f"CLOSING {app_name}", text=True)
+    except AppOpener.features.AppNotFound:
+        speak("The application is not running.", text=True)
 
 # Function to search on Wikipedia
 def search(text):
@@ -68,70 +120,13 @@ def search(text):
     else:
         script = f"Wikipedia page not found for: {search_term}"
         speak(script)
-        print(script)
 
-# Function to check if the text is a math quiz
-def is_math_qz(text):
-    pattern = r"(^\d+\.?(?:\d+)?\s[\+\-\*\/]{1}\s\d+\.?(?:\d+)?$)"
-    if match := re.search(pattern, text):
-        result = eval(match.group(1))
-        speak(f"'{match.group(1)}' = '{result}'", text=True)
-
-# Function to open an application
-def open_app(text):
-    app_name = text.replace("open", "").strip()
-    try:
-        AppOpener.open(app_name, match_closest=True, throw_error=True)
-        speak(f"OPENING {app_name}")
-    except AppOpener.features.AppNotFound:
-        speak("The application is not on your device.", text=True)
-
-# Function to close an application
-def close_app(text):
-    app_name = text.replace("close", "").strip()
-    try:
-        AppOpener.close(app_name, throw_error=True)
-        speak(f"CLOSING {app_name}")
-    except AppOpener.features.AppNotFound:
-        speak("The application is not running.", text=True)
-
-# Function to check for exit commands
-def is_exit(text):
-    if 'exit' in text or 'quit' in text:
-        script = "Exiting program..."
-        speak(script)
-        exit()
-
-# Function to guide the flow of the program
-def guide_flow(text):
-    pattern = r"(?:open|close|search)\s.+"
-    if match := re.search(pattern, text):
-        if "open" in text:
-            open_app(text)
-        elif "close" in text:
-            close_app(text)
-        elif "search" in text:
-            speak("SEARCHING")
-            search(text)
-
-# Function to respond to the user
-def respond(start=False):
-    while start:
-        audio_input = capture_audio(output=True)
-        if audio_input:
-            text = transcribe_speech(audio_input).lower()
-            is_exit(text)
-            if "pause" in text or "stop" in text:
-                speak("Paused for a while.")
-                main()
-            guide_flow(text)
-            is_math_qz(text)
 
 # Main function to activate the voice assistant
 def main():
     print("Say 'HELLO' to activate the voice assistant! Otherwise 'PAUSE' or 'STOP'.")
     while True:
-        audio = capture_audio(output=False, a_speech_time=None)
+        audio = capture_audio(output=False, a_speech_time=5)
         text = transcribe_speech(audio).lower()
         if "hello" in text:
             speak("Hello, I am your assistant! How can I help you?", text=True)
