6ecb9bc461a22f4fc9ad61dac8aaac4e5fd342f5
Kazu <114047336+WaiHlyanZ@users.noreply.github.com>
2024-05-22 06:53:58 +0630

    Latest version before recoding for Coolest project

---
+++ README.md
@@ -0,0 +1,83 @@
+# Voice Assistant
+
+### Video Demo:
+
+### Description
+
+Hello, your assistant is here!
+
+Perform some tasks only using your voice.
+
+For example, open/close  the application on your device.
+
+The only thing you need is your voice (speech) and keywords in its.
+
+### Features
+
+* Open `application` ✅
+* Close `application` ✅
+* Search `something` ✅
+* Calculate `Math` ✅
+
+### How to use?
+
+Start saying `<hello>` to activate the Voice Assistant!
+
+##### When to give command
+
+If the `Listening...` output is appeared on the CMD, it is ready to take the command.
+
+The assistant will listen for **three seconds** when `Listening...`  is showed.
+
+Suggestion: You should give command only `Listening...` is appear on the screen (CMD).
+
+##### Want to stop/pause, exit/quit
+
+It is pretty straight forward, just say them to perfom their jobs.
+
+Note: If you want to restart the assistant after stop/pause. Then say `<hello>` again.
+
+##### To boost the assistant
+
+Output nothing?
+Press the space bar on your keyboard in order to speed up the program.
+
+##### Usages of features
+
+###### Open/Close `<application>`
+
+By saying the keyword `<open>` followed by the application name to start the app.
+For example: speak clearly into you microphone "open chrome".
+
+###### Search
+
+To search on typical Web Browser (Chrom), just say `<search> you_want_to_search`.
+
+But if you would like to search on WikiPedia, say `<search> topic` `<on wiki>`
+
+###### Calculate Math
+
+You just need to say that mathematical expressions.
+
+For example, say `<three plus four>` to evaluate "3 + 4 = 7".
+
+##### Requirements
+
+1. Not in a noisy place
+2. Microphone should be work properly
+3. Use keywords
+4. Command only when `Listening...` is appear if not boost
+
+##### Warnings⚠
+
+PLEASE REMEMBER! This Voice Assistant is keyword based model.
+
+Outputs will appear. For example: `Cannot hear clearly.` from VA.
+
+Note: the `<exit/quit>` is completely exit the program. If you want to restart the Voice Assistant, you may need to rerun the program.
+
+### Contact
+
+##### Actors: [@MahnThuTa](https://github.com/MahnThuTa), [@WaiHlyanZ](https://github.com/WaiHlyanZ)
+
+##### GitHub Repo: [https://github.com/WaiHlyanZ/Voice-assistant.git](https://github.com/WaiHlyanZ/Voice-assistant.git)
--- main/main.py
+++ main/main.py
@@ -1,89 +1,156 @@
 import re
 import speech_recognition as sr
-from AppOpener import open, close
+import AppOpener
 import wikipediaapi
 import webbrowser
+import pyttsx3
+import urllib.parse
 
-# Initialize the recognizer
+# Initialize the recognizer and the speech engine
 recognizer = sr.Recognizer()
+engine = pyttsx3.init()
 
 
+## Features
 
-# Function to capture audio from the microphone
-def capture_audio():
-    with sr.Microphone() as source:
-        print("Listening...")
-        # Adjust for ambient noise
-        recognizer.adjust_for_ambient_noise(source)
-        try:
-            audio = recognizer.listen(source, timeout=5)
-            return audio
-        except sr.WaitTimeoutError:
-            print("Timeout occurred while listening.")
-
-# Function to transcribe speech using Google Speech Recognition
-def transcribe_speech(audio):
+# Function to open an application
+def open_app(text):
+    app_name = text.replace("open", "").strip()
     try:
-        # Use Google Speech Recognition
-        text = recognizer.recognize_google(audio)
-        print(f"Your Speech: {text}")
-        return text
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Please check your internet connection.") # auto pause the listening if the user don't use this app for a while
-        print("Error fetching results; {0}".format(e))
+        AppOpener.open(app_name, match_closest=True, throw_error=True)
+        speak(f"opening {app_name}")
+    except AppOpener.features.AppNotFound:
+        speak("The application is not on your device.", text=True)
 
-# Analize text
-key_word = ["open", "close", "search", "tell", "story", "note", "type"]
-def sanitize_text(text: str):
-    pattern = r"\b(?:open|close|search)\s(?:\w+)"
-    if match:= re.findall(pattern, text, re.IGNORECASE):
-        tasks = []
-        for pair in match:
-            action, app = pair.lower().split()
-            tasks.append((action, app))
-        return tasks
-    else:
-        return []
+# Function to close an application
+def close_app(text):
+    app_name = text.replace("close", "").strip()
+    try:
+        AppOpener.close(app_name, throw_error=True)
+        speak(f"CLOSING {app_name}", text=True)
+    except AppOpener.features.AppNotFound:
+        speak("The application is not running.", text=True)
 
+# Function to search on Web browser
+def web_search(text):
+    search_term = text.replace("search", "").strip()
+    script = f"Searching {search_term} on web browser"
+    speak(script, text=True)
+    query = urllib.parse.quote(search_term)
+    web_search_url = f"https://www.google.com/search?q={query}"
+    webbrowser.open(web_search_url)
 
-# Perform an action 
-def app_op(task: tuple):
-    act, app = task
-    if act == "open": open(app)
-    if act == "close": close(app)
-
-    
-
-# Search Wiki
-def is_search():
-    pass
-
-def search(text: str): # search Elon Muk
+# Function to search on Wikipedia
+def wiki_search(text):
+    search_term = text.strip("search").rstrip("on wikipedia")
     
     wiki_wiki = wikipediaapi.Wikipedia("Voice Assistant")
-    page = wiki_wiki.page(text)
+    page = wiki_wiki.page(search_term)
+    
     if page.exists():
-        print("Page exists!")
-        print("Opening Wikipedia page:", page.fullurl)
+        script = f"Opening Wikipedia page for: {search_term}"
+        speak(script, text=True)
+        print("Page URL:", page.fullurl)
         webbrowser.open(page.fullurl)
     else:
-        print("Page not found.")
+        script = f"{search_term} not found on WikiPedia"
+        speak(script, text=True)
+    
+
+## System
+
+# Function to capture audio from the microphone
+def capture_audio(output=True, a_speech_time=3):
+    with sr.Microphone() as source:
+        if output:
+            print("\nListening...\n")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, phrase_time_limit=a_speech_time)
+        return audio
+
+# Function to transcribe speech using Google Speech Recognition
+def transcribe_speech(audio, output=True, error=True):
+    try:
+        text = recognizer.recognize_google(audio)
+        if output:
+            print(f"Your Speech: {text}")
+        return text
+    except sr.UnknownValueError:
+        if error:
+            print("Cannot hear clearly.")
+    except sr.RequestError as e:
+        script = "Please check your internet connection."
+        speak(script, text=True)
+    return ""
 
 
-def main():
-    while True:
-        audio_input = capture_audio()
+# Function to speak to the user
+def speak(script, text=False):
+    engine.say(script)
+    if text:
+        print(script)
+    engine.runAndWait()
+
+# Function to check for exit commands
+def is_exit(text):
+    if 'exit' in text or 'quit' in text:
+        script = "Exiting program..."
+        speak(script, text=True)
+        exit()
+
+# Function to check if the text is a math quiz
+def is_math_qz(text):
+    try:
+        result = eval(text)
+        speak(f"{text} = {result}", text=True)
+    except Exception:
+        print("\nSyntaxError")
+
+
+# Function to guide the flow of the program
+def guide_flow(text):
+    pattern = r"(?:open|close|search)\s.+"
+    if re.search(pattern, text):
+        if "open" in text:
+            open_app(text)
+        elif "close" in text:
+            close_app(text)
+        elif re.search(r".+on\swiki(?:pedia)?", text):
+            wiki_search(text)
+        elif "search" in text:
+            speak("SEARCHING", text=True)
+            web_search(text)
+    elif re.match(r"^[\d\s\+\-\*\/\(\)\.]+$", text):
+        is_math_qz(text)
+
+
+# Function to respond to the user
+def respond(start=False):
+    while start:
+        audio_input = capture_audio(output=True)
         if audio_input:
-            text = transcribe_speech(audio_input)
-            if text: 
-                tasks = sanitize_text(text)
-                for task in tasks:
-                    app_op(task)
-                if "search" in text:
-                    
-                    search(text)
+            text = transcribe_speech(audio_input).lower()
+            is_exit(text)
+            
+            if "pause" in text or "stop" in text:
+                speak("Paused for a while.", text=True)
+                # Switch to background process
+                main()
+                # Break innter loop to prevent loopings
+                break
+            guide_flow(text)
+
+
+# Main function to activate the voice assistant
+def main(text=True):
+    print("Say 'HELLO' to activate the voice assistant! Otherwise 'PAUSE' or 'STOP'.")
+    while True:
+        audio = capture_audio(output=False, a_speech_time=3)
+        text = transcribe_speech(audio, error=False).lower()
+        if "hello" in text:
+            speak("Hello, I am your assistant! How can I help you?", text=True)
+            respond(start=True)
+        is_exit(text)
 
 if __name__ == "__main__":
     main()
